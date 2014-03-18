1678da43c0b738c71e5119975234e92e57dc05d1
domhanak <domin.hanak@gmail.com>
2014-03-18 15:03:18 +0100

    added Song entity implementation

---
+++ src/main/java/cz/muni/fi/pv168/musicManager/Song.java
@@ -0,0 +1,90 @@
+package cz.muni.fi.pv168.musicManager;
+
+/**
+ * Represents a Song.
+ *
+ * Created by Dominik Hanak on 5.3.2014.
+ */
+public class Song {
+    private long songId;
+    private String name;
+    private int track;
+    private int rank;
+    private int length;
+
+    public Song() {}
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Song{");
+        sb.append("songId=").append(songId);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", track=").append(track);
+        sb.append(", rank=").append(rank);
+        sb.append(", length=").append(length);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Song song = (Song) obj;
+
+        if (songId != song.songId) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (songId ^ (songId >>> 32));
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public long getSongId() {
+        return songId;
+    }
+
+    public void setSongId(long songId) {
+        this.songId = songId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTrack() {
+        return track;
+    }
+
+    public void setTrack(int track) {
+        this.track = track;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
